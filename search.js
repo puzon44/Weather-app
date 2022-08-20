186bba0c860c7f126c2cb20a4328d86ddd1c5825
puzon44 <puzonpathak2001@gmail.com>
2022-08-20 20:57:18 +0545

    1st weather app prototype

---
+++ search.js
@@ -0,0 +1,50 @@
+const searchBar = document.getElementById("searchBar");
+const searchButton = document.getElementById("searchButton");
+const city = document.querySelector(".city");
+const cityTemp = document.querySelector(".temp");
+const weatherIcon = document.getElementById("weatherIcon");
+const weatherStatus = document.querySelector(".status");
+const Humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind-Speed");
+
+
+const apikey = "f2d06b7a960941702742599a5fd50c1e";
+
+function displayWeather(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+
+  city.innerText = name;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  let temprature = temp - 273.15;
+  cityTemp.innerText = `${Math.floor(temprature)}° C`;
+  weatherStatus.innerHTML = `Weather-Status: ${description}`;
+  Humidity.innerText = `Humidity: ${humidity} %`;
+  wind.innerText = `Wind-Speed: ${speed} km/h`;
+}
+
+function fetchWeather(city) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apikey
+  )
+    .then((response) => response.json())
+    .then((data) => displayWeather(data))
+    .catch(() => alert("No Data found" + " 404"));
+}
+
+// code for serach button
+
+searchButton.addEventListener("click", () => {
+  fetchWeather(searchBar.value);
+});
+
+searchBar.addEventListener("keyup", (event) => {
+  if (event.key == "Enter") {
+    fetchWeather(searchBar.value);
+  }
+});
